ea55fd5d0d531c44eeebd09e6177d5d8d5a2e27e
szkj-web-he <83932083+szkj-web-he@users.noreply.github.com>
2022-06-07 14:49:25 +0800

    1

--- src/storageCabinet.tsx
+++ src/storageCabinet.tsx
@@ -1,6 +1,6 @@
 /* <------------------------------------ **** DEPENDENCE IMPORT START **** ------------------------------------ */
 /** This section will include all the necessary dependence for this tsx file */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useMContext } from "./context";
 import { comms } from ".";
 /* <------------------------------------ **** DEPENDENCE IMPORT END **** ------------------------------------ */
@@ -24,13 +24,13 @@ export const StorageCabinet: React.FC = () => {
     /* <------------------------------------ **** STATE START **** ------------------------------------ */
     /************* This section will include this component HOOK function *************/
 
-    const { isMobile } = useMContext();
+    const { isMobile, basketFn } = useMContext();
 
     const [is1024, setIs1024] = useState(window.matchMedia("(max-width: 1000px)").matches);
 
     const [is375, setIs375] = useState(window.matchMedia("(max-width: 703px)").matches);
 
-    const [list, setList] = useState<Array<ListItemProps>>(
+    const listRef = useRef<Array<ListItemProps>>(
         comms.config.options
             ? deepCloneData(comms.config.options[0]).map((item) => ({
                   code: item.code,
@@ -38,10 +38,66 @@ export const StorageCabinet: React.FC = () => {
               }))
             : [],
     );
+    const [list, setList] = useState([...listRef.current]);
+
+    const indexRef = useRef<number>();
+    const [activeIndex, setActiveIndex] = useState(indexRef.current);
 
     /* <------------------------------------ **** STATE END **** ------------------------------------ */
     /* <------------------------------------ **** PARAMETER START **** ------------------------------------ */
     /************* This section will include this component parameter *************/
+    useLayoutEffect(() => {
+        let timer: null | number = null;
+        const findIndex = (x: number, y: number) => {
+            const els = document.elementsFromPoint(x, y);
+            let n: number | null = null;
+            for (let i = 0; i < els.length; ) {
+                const el = els[i];
+                const classAttr = el.getAttribute("class")?.split(" ");
+                if (classAttr?.includes("storageCabinet_item")) {
+                    n = Number(el.getAttribute("data-i"));
+                    i = els.length;
+                } else {
+                    ++i;
+                }
+            }
+            return n;
+        };
+
+        basketFn.current = {
+            move(x, y) {
+                timer && window.clearTimeout(timer);
+                timer = window.setTimeout(() => {
+                    const n = findIndex(x, y);
+
+                    indexRef.current = n ?? undefined;
+                    setActiveIndex(indexRef.current);
+                });
+            },
+            up(res) {
+                timer && window.clearTimeout(timer);
+                indexRef.current = undefined;
+                setActiveIndex(indexRef.current);
+                const n = findIndex(res.x, res.y);
+                if (n === res.index) {
+                    return;
+                }
+
+                const arr = [...listRef.current];
+                if (typeof res.index === "number") {
+                    arr[res.index].value = undefined;
+                }
+                if (typeof n === "number") {
+                    arr[n].value = {
+                        code: res.data.code,
+                        content: res.data.content,
+                    };
+                }
+                listRef.current = [...arr];
+                setList([...listRef.current]);
+            },
+        };
+    }, [basketFn]);
 
     useEffect(() => {
         const fn = () => {
@@ -58,44 +114,20 @@ export const StorageCabinet: React.FC = () => {
     /* <------------------------------------ **** FUNCTION START **** ------------------------------------ */
     /************* This section will include this component general function *************/
 
-    const handleColorChange = (res: ListItemProps[]) => {
-        setList([...res]);
-        const data: Record<string, string | undefined> = {};
-        for (let i = 0; i < res.length; i++) {
-            data[res[i].code] = res[i].value?.code;
-        }
-        comms.state = data;
-    };
-
     let classStr = "storageCabinet_wrap";
     let mainEl = <></>;
 
     if (isMobile) {
         mainEl = is375 ? (
-            <Mobile colors={list} />
+            <Mobile colors={list} activeIndex={activeIndex} />
         ) : (
-            <Tablet
-                value={valueData}
-                handleChange={handleChange}
-                handleColorChange={handleColorChange}
-                colors={list}
-            />
+            <Tablet colors={list} activeIndex={activeIndex} />
         );
     } else {
         mainEl = is1024 ? (
-            <SmallDesk
-                value={valueData}
-                handleChange={handleChange}
-                handleColorChange={handleColorChange}
-                colors={list}
-            />
+            <SmallDesk colors={list} activeIndex={activeIndex} />
         ) : (
-            <Desk
-                value={valueData}
-                handleChange={handleChange}
-                handleColorChange={handleColorChange}
-                colors={list}
-            />
+            <Desk colors={list} activeIndex={activeIndex} />
         );
     }
 
